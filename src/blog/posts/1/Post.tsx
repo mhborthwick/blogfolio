929b7a30e795f7d31bb38f9aa8d36e159fae3dcf
mhborthwick <mhborthwick@gmail.com>
2022-11-11 13:52:06 -0800

    feat: start to add blog posts

---
+++ src/blog/posts/1/Post.tsx
@@ -0,0 +1,11 @@
+import "../../../blog/Blog.css";
+
+function Post() {
+  return (
+    <div>
+      <h1>Post</h1>
+    </div>
+  );
+}
+
+export default Post;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import { resolve } from "path";
+import glob from "glob";
+import { extname, relative, resolve } from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
+const blogPosts = Object.fromEntries(
+  glob
+    .sync("src/blog/posts/**/*.html")
+    .map((file) => [
+      relative("src", file.slice(0, file.length - extname(file).length)),
+      fileURLToPath(new URL(file, import.meta.url)),
+    ])
+);
 
 export default defineConfig({
   root,
@@ -15,6 +25,7 @@ export default defineConfig({
       input: {
         main: resolve(root, "index.html"),
         portfolio: resolve(root, "portfolio", "index.html"),
+        ...blogPosts,
       },
     },
   },
