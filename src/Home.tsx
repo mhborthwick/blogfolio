f0941da38c05f01ad41b399ea1d290101b10da45
mhborthwick <mhborthwick@gmail.com>
2022-11-11 12:13:57 -0800

    feat: start to set up homepage

---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>Homepage</h1>
+    </div>
+  );
+}
+
+export default Home;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+
 export default defineConfig({
-  plugins: [react()]
-})
+  root,
+  plugins: [react()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+      },
+    },
+  },
+});
