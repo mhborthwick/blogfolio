a95f9c8c184d07d84985c749deefeaa89ccdd7fe
mhborthwick <mhborthwick@gmail.com>
2022-11-13 13:58:03 -0800

    feat: make mobile nav better

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,34 +1,62 @@
 import {
-  Box,
+  Divider,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
   IconButton,
   Link,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
+  useDisclosure,
+  Button,
 } from "@chakra-ui/react";
-import { BiMenuAltRight } from "react-icons/bi";
+import React, { type MutableRefObject } from "react";
+import { RiMenu3Fill, RiMenuFill } from "react-icons/ri";
 
 function MobileNav() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef() as MutableRefObject<HTMLButtonElement>;
+
   return (
-    <Box as="nav" m="auto 0">
-      <Menu>
-        <MenuButton
-          as={IconButton}
-          aria-label="Options"
-          icon={<BiMenuAltRight size="2em" />}
-          variant="outline"
-        />
-        <MenuList>
-          <MenuItem as="a" href="/about">
-            About
-          </MenuItem>
-          <MenuItem as="a" href="/portfolio">
-            Portfolio
-          </MenuItem>
-        </MenuList>
-      </Menu>
-    </Box>
+    <>
+      <Button
+        ref={btnRef}
+        onClick={onOpen}
+        as={IconButton}
+        aria-label="Options"
+        icon={<RiMenu3Fill size="1.5em" />}
+        variant="link"
+      />
+      <Drawer
+        isOpen={isOpen}
+        size="full"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerCloseButton size="lg" />
+          </DrawerHeader>
+          <DrawerBody
+            display="flex"
+            flexDirection="column"
+            lineHeight="1.75"
+            fontWeight="bold"
+          >
+            <Link href="/about" mt="1.25em" mb="1.25em">
+              About
+            </Link>
+            <Divider />
+            <Link href="/portfolio" mt="1.25em" mb="1.25em">
+              Portfolio
+            </Link>
+            <Divider />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 }
 
