c2909e36ca95d4885a62be0a9add6f013d16dc5e
mhborthwick <mhborthwick@gmail.com>
2022-11-18 16:24:44 -0800

    fix: add ColorModeScript to post

--- src/pages/blog/posts/1/main.tsx
+++ src/pages/blog/posts/1/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import theme from '../../../../theme'
 import Content from './Content.mdx'
 import PostWrapper from '../PostWrapper'
@@ -8,6 +8,7 @@ import PostWrapper from '../PostWrapper'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <PostWrapper id={1}>
         <Content />
       </PostWrapper>
